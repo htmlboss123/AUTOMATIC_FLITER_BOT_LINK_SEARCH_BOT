ca3bbc788d6afa3702bf8e6cf6ebb5d760edd239
htmlboss123 <91206261+htmlboss123@users.noreply.github.com>
2023-09-22 20:07:36 -0700

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ API_ID       = 21956488
 API_HASH     = "812529f879f06436925c7d62eb49f5d1"
 BOT_TOKEN    = "6358913868:AAFZ5ngQ3kMcFwQct8G72tWbHSBH8ljiX-w"
 SESSION      = "BQFPB4gAVetaaeQzdPJRaCwgqVD-KNkMkJ59egCvywg6_qiZqU0O2najP3apnOEcgFIbBlk74tZabvvWqNHYNicbOO7Vmc7RBdHjdfVTtgyMwgvIwbop2QyZ3S0kcCgGQUBFwssdmnQwAoYhRuAHT_Cs5YYud4oywuuNOSIHBO8f1kK-oKcBYPbK2sZgsDe10kE2jcKYo2RMHM4XuaQJVzGfRHj1s06uO0Om_k6G0bDVmwif5tXNMjzck_D-Oik4hTD7azMxqSfKVd-43xKO18z79gs6qlen0ywPE3LQ5NFwdoyuSTNzO_2EFKWsP9ce7-PD-vMHPk1LyKDBdHcWezaEODDsAAAAAAB4ai0IAA"
-DATABASE_URI = "mongodb+srv://ultra_pro_max:6395630590@cluster0.5vw8g2h.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://ultra_pro_max:<6395630590>@cluster0.5vw8g2h.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001876702744
 ADMIN        = 2020224264
 CHANNEL      = "@VIP_BRO10"
